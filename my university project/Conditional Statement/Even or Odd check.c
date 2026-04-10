e51d1aeee916c0577f6baca5d92268607e4e261d
MD Saiful Islam <sarkarsaiful2005@gmail.com>
2026-04-10 22:53:01 +0600

    Update Even or Odd check.c

--- my university project/Conditional Statement/Even or Odd check.c
+++ my university project/Conditional Statement/Even or Odd check.c
@@ -7,9 +7,9 @@ Expected Output :
 int main (){
     int a;
     scanf ("%d",&a);
-    if(a%2!=0)
+    if(a%2==0)
         printf("%d, is an even integer",a);
     else
-        printf("%d is an even integer");
+        printf("%d, is an odd integer");
     return 0;
 }
