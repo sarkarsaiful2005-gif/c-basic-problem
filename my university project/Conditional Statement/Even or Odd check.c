c2148429f89c34380d22ab16eb16812024fd906d
MD Saiful Islam <sarkarsaiful2005@gmail.com>
2026-04-10 20:53:20 +0600

    Add files via upload

---
+++ my university project/Conditional Statement/Even or Odd check.c
@@ -0,0 +1,15 @@
+/*Write a C program to check whether a given number is even or odd.
+Test Data : 15
+Expected Output :
+15 is an odd integer*/
+
+#include<stdio.h>
+int main (){
+    int a;
+    scanf ("%d",&a);
+    if(a%2!=0)
+        printf("%d, is an even integer",a);
+    else
+        printf("%d is an even integer");
+    return 0;
+}
