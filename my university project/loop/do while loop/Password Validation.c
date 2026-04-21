05832043270bebe49fb2b3b4e808b1592b6a61df
MD Saiful Islam <sarkarsaiful2005@gmail.com>
2026-04-21 20:24:29 +0600

    Add files via upload

---
+++ my university project/loop/do while loop/Password Validation.c
@@ -0,0 +1,25 @@
+/*Write a C program that prompts the user to enter a password.
+ Use a do-while loop to keep asking for the password until the correct one is entered.*/
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char p[50],r[60];
+    printf ("make your password without space:");
+    scanf("%s",p);
+    printf("\n");
+    do{
+        printf("Enter your passward:");
+        scanf("%s",r);
+        if(strcmp(p,r)==0)
+        {
+            printf("CORRECT PASSWORD\n");
+            break;
+        }
+        else
+        {
+            printf("WORNG PASSWORD\n");
+        }
+    }while(1);
+    return 0;
+}
