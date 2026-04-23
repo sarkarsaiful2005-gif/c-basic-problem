50fc68daa7e66003054a808f9f99c49d76a497e2
MD Saiful Islam <sarkarsaiful2005@gmail.com>
2026-04-23 20:37:48 +0600

    Update Vertical Multiplier Table from 1 to n.c

--- my university project/loop/For loop/Vertical Multiplier Table from 1 to n.c
+++ my university project/loop/For loop/Vertical Multiplier Table from 1 to n.c
@@ -6,15 +6,7 @@ Multiplication table from 1 to 8
 1x1 = 1, 2x1 = 2, 3x1 = 3, 4x1 = 4, 5x1 = 5, 6x1 = 6, 7x1 = 7, 8x1 = 8
 ...
 1x10 = 10, 2x10 = 20, 3x10 = 30, 4x10 = 40, 5x10 = 50, 6x10 = 60, 7x10 = 70, 8x10 = 80*/
-/*Write a program in C to display the multiplication table for a given integer.
-Test Data :
-Input the number (Table to be calculated) : 15
-Expected Output :
-15 X 1 = 15
-...
-...
-15 X 10 = 150
-*/
+
 #include<stdio.h>
 int main()
 {
